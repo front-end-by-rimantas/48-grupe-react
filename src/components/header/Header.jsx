ce528d9a5803902f49ecb874f558cb5a087de0b3
Rimantas <belauzas@gmail.com>
2024-03-20 13:12:27 +0200

    Up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import logo from '../../logo.svg';
+import style from './Header.module.css';
+
+function Header() {
+    // class="red {style.mainHeader}"
+    return (
+        <header className={style.mainHeader + ' grey-bg'}>
+            <img className={style.logo} src={logo} alt="Logo" />
+            <nav className={style.mainNav}>
+                <a className={style.link} href="/">Home</a>
+                <a className={style.link} href="/">About</a>
+                <a className={style.link} href="/">Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export { Header };
